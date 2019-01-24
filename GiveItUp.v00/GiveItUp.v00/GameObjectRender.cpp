2fc4f1e8e221fe7f5789001a5bd120c4ce06fb34
Tai Nguyen <nguyenductai101098@gmail.com>
2019-01-24 09:44:26 +0700

    + delete old source

--- GiveItUp.v00/GiveItUp.v00/GameObjectRender.cpp
+++
@@ -1,9 +0,0 @@
-#include "stdafx.h"
-#include "GameObjectRender.h"
-
-void GameObjectRender::Init(const char* textureName, int x, int y){
-	texture.loadFromFile(textureName);
-	sprite.setTexture(texture);
-	sprite.setPosition(x, y);
-
-}
--- GiveItUp.v00/GiveItUp.v00/mc.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-class mc
-{
-public:
-	mc();
-	~mc();
-};
-
